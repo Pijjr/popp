e7fde112e2f2994d83223082db788e0f757b5875
Pijjr <94920045+Pijjr@users.noreply.github.com>
2023-08-26 00:24:49 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+import '../scss/style.scss'
+import '../js/swiper-repair-brands'
+import '../js/show-brands'
+import '../js/show-text'
+import '../js/swiper-repair-devices'
+import '../js/show-devices'
+import '../js/swiper-price-list'
+import '../js/burger-menu'
+import '../js/show-modal'
+console.log('Works!')
